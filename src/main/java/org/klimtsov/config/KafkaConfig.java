03d7f686d76dcf1760b457dd5f51f1b33652479b
Klimtsov Konstantin <kamagoroff@gmail.com>
2025-12-13 22:13:06 +0300

    Fixes.

---
+++ src/main/java/org/klimtsov/config/KafkaConfig.java
@@ -0,0 +1,30 @@
+package org.klimtsov.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic userEventsTopic() {
+        return TopicBuilder.name("user-events")
+                .partitions(1)  //1 партиция (для простоты).
+                .replicas(1)    //1 реплика
+                .build();
+    }
+}
--- src/main/java/org/klimtsov/kafka/KafkaProducer.java
+++ src/main/java/org/klimtsov/kafka/KafkaProducer.java
@@ -1,17 +1,38 @@
 package org.klimtsov.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.klimtsov.dto.UserEvent;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
     private static final String TOPIC = "user-events";
 
     public void sendUserEvent(UserEvent userEvent) {
-        kafkaTemplate.send(TOPIC, userEvent);
+        log.info("Отправляю событие в Kafka: {}", userEvent);
+
+        try {
+            CompletableFuture<SendResult<String, Object>> future =
+                    kafkaTemplate.send(TOPIC, userEvent);
+
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    log.info("✅ Событие отправлено в Kafka. Топик: {}, offset: {}",
+                            TOPIC, result.getRecordMetadata().offset());
+                } else {
+                    log.error("❌ Ошибка отправки в Kafka: {}", ex.getMessage());
+                }
+            });
+        } catch (Exception e) {
+            log.error("❌ Критическая ошибка при отправке в Kafka: {}", e.getMessage());
+        }
     }
 }
